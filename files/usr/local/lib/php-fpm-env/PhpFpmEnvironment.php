4db5eec56beb8d56c03744e64b15b304b8aa47c0
Dereckson <dereckson@espace-win.org>
2015-04-21 03:30:03 +0200

    Environment propagation: php-fpm
    We now also propagate the environment to php-fpm, so the web application
    can get access to the variables set in container.
    
    This allows to configure the application at docker run step if it follows
    the http://12factor.net/config recommandation.
    
    We don't use `clear_env = false` but filter the environment with a PHP
    script, to discard the MYSQL_ENV_MYSQL_ROOT_PASSWORD variable, introduced
    by the MySQL official Docker image if we link a container to the web one
    using --link <MySQL container name>:mysql (Docker propagates environment
    from a container to linked containers).
    
    In files/usr/local/lib/php-fpm-env/PhpFpmEnvironment.php, behavior could so
    be customized to prepare an environment with the desired level of complexity.

---
+++ files/usr/local/lib/php-fpm-env/PhpFpmEnvironment.php
@@ -0,0 +1,76 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * Allows to get the content of a php-fpm environment configuration file
+ */
+class PhpFpmEnvironment {
+    /**
+     * The temporary directory, used in TMP, TEMP and TMPDIR environment variables
+     * @var string
+     */
+    const TMP = '/tmp';
+
+    /**
+     * The path where to find executables, where sbin should be excluded if you don't run PHP as root.
+     * @var string
+     */
+    const PATH = '/usr/local/bin:/usr/bin:/bin';
+
+    /**
+     * The environment variables to discard
+     * @var Array
+     */
+    const VARIABLES_TO_DISCARD = [
+        '_',           // The caller executable script, not pertinent
+        'HOME',        // Set correctly by php-fpm
+        'TERM',        // Not pertinent in server context
+        'MYSQL_ENV_MYSQL_ROOT_PASSWORD', // from --link …:mysql
+    ];
+
+    /**
+     *ets an environment array from the current process environment,
+     * with PATH and temp variablesfiltered.
+     *
+     * @return Array
+     */
+    public static function getEnvironmentVariables () {
+        $variables = [];
+
+        foreach ($_ENV as $key => $value) {
+            if (static::mustIgnoreVariable($key)) {
+                continue;
+            }
+
+            $variables[$key] = $value;
+        }
+
+        $variables['PATH'] = static::PATH;
+        $variables['TMP'] = static::TMP;
+        $variables['TEMP'] = static::TMP;
+        $variables['TMPDIR'] = static::TMP;
+
+        return $variables;
+    }
+
+    /**
+     * Determines if the variable name must be ignored
+     *
+     * @return bool true if the variable must be ignored; otherwise, false.
+     */
+    public static function mustIgnoreVariable ($variableName) {
+        return in_array($variableName, static::VARIABLES_TO_DISCARD);
+    }
+
+    /**
+     * Gets the environment
+     */
+    public static function get () {
+        $variables = static::getEnvironmentVariables();
+        foreach ($variables as $key => $value) {
+            echo 'env["', $key, '"] = "', $value, '"', PHP_EOL;
+        }
+    }
+}
+
+PhpFpmEnvironment::get();
